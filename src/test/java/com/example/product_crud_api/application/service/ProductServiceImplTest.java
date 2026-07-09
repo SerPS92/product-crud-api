5ffc222284ae9cf016395a6bb4b452dee6db10ac
Sergio <sergio.rianxo92@gmail.com>
2026-07-09 19:06:23 +0200

    Add README and Postman collection

--- src/test/java/com/example/product_crud_api/application/service/ProductServiceImplTest.java
+++ src/test/java/com/example/product_crud_api/application/service/ProductServiceImplTest.java
@@ -1,6 +1,10 @@
 package com.example.product_crud_api.application.service;
 
-import static org.junit.jupiter.api.Assertions.*;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.verify;
